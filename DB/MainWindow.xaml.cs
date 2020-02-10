46ca6433020c07ad8a26228ece84c2bced85fb5f
Randy DePriest <jrd100299@gmail.com>
2020-02-10 08:01:52 -0600

    finalizations

--- DB/MainWindow.xaml.cs
+++ DB/MainWindow.xaml.cs
@@ -22,42 +22,57 @@ namespace DB
     public partial class MainWindow : Window
     {
         OleDbConnection cn;
+
+        /* Description: Main window. Initializes the main window and connects to the employee database
+         * 
+         * Results: The main window is initializd and connected.
+         */
         public MainWindow()
         {
             InitializeComponent();
             cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jrd10\Documents\Database.accdb");
         }
 
+        /* Description: The button implimentation for the assets. Prints out all data from the assets table.
+         * 
+         * Results: The assets table has been printed out.
+         */
         private void SeeAssets_Click(object sender, RoutedEventArgs e)
         {
-            string query = "select * from Assets";
+            string query = "select EmployeeID, AssetID, Description, * from Assets";
             OleDbCommand cmd = new OleDbCommand(query, cn);
             cn.Open();
             OleDbDataReader read = cmd.ExecuteReader();
             string data = "";
             while (read.Read())
             {
-                data += read[0].ToString() + "\n";
+                data += read["EmployeeID"].ToString() + " ";
+                data += read["AssetID"].ToString() + " ";
+                data += read["Description"].ToString() + "\n";
             }
             TextArea.Text = data;
             cn.Close();
         }
 
+        /* Description: The button implimentation for the employees. Prints out all data from the employees table.
+         * 
+         * Results: The employees table has been printed out
+         */
         private void SeeEmployees_Click(object sender, RoutedEventArgs e)
         {
-            string query = "select * from Employee";
+            string query = "select EmployeeID, LastName, FirstName, * from Employee";
             OleDbCommand cmd = new OleDbCommand(query, cn);
             cn.Open();
             OleDbDataReader read = cmd.ExecuteReader();
             string data = "";
             while (read.Read())
             {
-                data += read[0].ToString() + "\n";
+                data += read["EmployeeID"].ToString() + " ";
+                data += read["LastName"].ToString() + " ";
+                data += read["FirstName"].ToString() + "\n";
             }
             TextArea.Text = data;
             cn.Close();
-            
-
         }
     }
 }
